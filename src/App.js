c7f0b076d03268cbfe052ea75082ea5e500d1285
thcho <thcho@uw.edu>
2021-02-24 01:59:39 -0800

    first version of meme search bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import styled from 'styled-components'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 function App() {
+  const [text, setText] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [memes, setMemes] = useState([])
+
+  async function getMemes(){
+    setMemes([])
+    setLoading(true)
+    let url = 'https://api.giphy.com/v1/gifs/search?'
+    url += 'api_key=' + 'jhQazp87aPuMIRIZoFu2kaI2Uk5GjZRJ'
+    url += '&q=' + text
+    const r = await fetch(url)
+    const j = await r.json()
+    setMemes(j.data)
+    setLoading(false)
+    setText('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Wrap>
+      <Header>
+        <TextField id="search for memes" label="Outlined" variant="outlined" style={{width:'calc(100% - 110px)'}} 
+          value={text} onChange={e=> setText(e.target.value)} autoFocus
+          onKeyPress={e=> e.key==='Enter' && getMemes()}
+        />
+        <Button variant="contained" color="primary" style={{height:55, marginLeft:10, width:100}} 
+        disabled={!text || loading} onClick={getMemes}>
+          Search
+        </Button>
+      </Header>
+      <Body>
+        {memes.map(m=> <Meme src={m.images.fixed_width.url} />)}
+      </Body>
+    </Wrap>
+  )
 }
 
+const Meme = styled.img`
+  max-height: 200px;
+  max-width: 200px;
+  min-width: 200px;
+  object-fit: cover;
+  margin:5px;
+`
+const Wrap = styled.div`
+  display:flex;
+  flex-direction:column;
+  width:100%;
+  height:100vh;
+`
+const Header = styled.header`
+  width:100%;
+  min-height:50px;
+  padding: 10px;
+  box-sizing: border-box;
+`
+const Body = styled.div`
+  width:100%;
+  display:flex;
+  flex-wrap:wrap;
+  flex:1;
+  overflow:auto;
+`
 export default App;
